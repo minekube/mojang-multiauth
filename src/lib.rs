49abdfc28ec3138099e91c0a111d9a189217ab67
julian-siebert <mail@julian-siebert.de>
2024-04-04 17:42:28 +0200

    Revert "feat: Tried to reimplement code in rust"
    This reverts commit 91c787e844508bcd89ce9bdb064911316ae258ed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,6 @@
 use worker::*;
 
-const NEW_URL_LIST: [&str; 2] = [
-    "https://sessionserver.mojang.com/session/minecraft/hasJoined",
-    "https://api.minehut.com/mitm/proxy/session/minecraft/hasJoined"
-];
-
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    let url = match req.url() {
-        Ok(url) => url,
-        Err(_) => {
-            return Response::error("Unrecognizable path", 404);
-        }
-    };
-    
-    if url.path() != "/mitm/session/minecraft/hasJoined" {
-        return Response::empty();
-    }
-    
-    println!("{}", url);
-    
-    
-    get_authenticated_user(url, env, ctx)
-}
-
-fn get_authenticated_user(url: Url, env: Env, ctx: Context) -> Result<Response> {
-    for new_url in NEW_URL_LIST {
-        let modified_request = match Request::new("", Method::Get) {
-            Ok(request) => request,
-            Err(err) => {
-                println!("Unable to modify request {url}: {err}");
-                return Response::error("", 204);
-            }
-        };
-        // todo
-    }
-    
-    return Response::error("", 204);
+    Response::ok("Hello, World!")
 }
